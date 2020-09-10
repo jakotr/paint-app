13652609acd54f95d9a4cc9e8e26f4355b2f5443
Jakub Otreba <jakub.otreba1@gmail.com>
2020-09-10 15:22:58 +0200

    add Listeners and change structure

---
+++ js/Tool.js
@@ -0,0 +1,11 @@
+// tools to select
+export default class Tool{
+
+    static TOOL_LINE = 'line';
+    static TOOL_RECTANGLE = 'rectangle';
+    static TOOL_CIRCLE = 'circle';
+    static TOOL_TRIANGLE = 'triangle';
+    static TOOL_PEN = 'pen';
+    static TOOL_BRUSH = 'brush';
+    static TOOL_PAINT = 'paint';
+}
---
+++ js/index.js
@@ -0,0 +1,49 @@
+// lineWidth check if its working with the brush or todo new one in html like 1407
+
+import Tool from './Tool.js';
+
+const icons = document.querySelectorAll('.group__icon');
+const lineWidthGroup = document.querySelector('.toolbox-left__line-options');
+const lineWidth = document.querySelectorAll('.group__line-width');
+const colorPick = document.querySelectorAll('.toolbox-right__colors__wrapper');
+// listener for icons on left toolbox (without the line width)
+icons.forEach(icon => {
+    icon.addEventListener('click', (e) => {
+        // activated selected icon to know what is selected ( without 2 first: undo & download)
+        if(!(icon.dataset.icon == 'undo' || icon.dataset.icon == 'download')) {
+            document.querySelector('[data-icon].active').classList.toggle('active');
+            icon.classList.toggle('active');
+        } 
+        
+        let selectedTool = icon.dataset.icon;
+            
+        switch (selectedTool) {
+            case Tool.TOOL_LINE:
+            case Tool.TOOL_RECTANGLE:
+            case Tool.TOOL_CIRCLE:
+            case Tool.TOOL_TRIANGLE:
+            case Tool.TOOL_PEN:
+            case Tool.TOOL_BRUSH:
+                lineWidthGroup.classList.add('display');
+            break;
+            default: 
+                lineWidthGroup.classList.remove('display');
+        }
+    });
+});
+
+//listener for lineWIdth in the left toolbox
+lineWidth.forEach(width => {
+    width.addEventListener('click', () => {
+        document.querySelector('[data-line-width].active').classList.toggle('active');
+        width.classList.toggle('active');
+    });
+});
+
+//listener for every color in the right toolbox 
+colorPick.forEach(color => {
+    color.addEventListener('click', () => {
+        document.querySelector('[data-color].active').classList.toggle('active');
+        color.classList.toggle('active');
+    });
+});
